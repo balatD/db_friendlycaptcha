522ee56ad0d86f50a4f4eca6d5d1582e896887f4
Sebastian Fischer <typo3@evoweb.de>
2023-08-29 08:36:04 +0200

    Additional path fixes

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,6 +6,6 @@ defined('TYPO3') or die();
 
 call_user_func(function () {
     ExtensionManagementUtility::addTypoScriptSetup('
-module.tx_form.settings.yamlConfigurations.1974 = EXT:friendlycaptcha/Configuration/Yaml/FormSetup.yaml
+module.tx_form.settings.yamlConfigurations.1998 = EXT:friendlycaptcha/Configuration/Yaml/FormSetup.yaml
     ');
 });
